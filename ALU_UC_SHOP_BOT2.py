ec4b907f171b90c03cb86e28562ca4fc55e420d3
vremenniyackaunt411-png <vremenniyackaunt411@gmail.com>
2026-09-05 06:08:29 +0500

    Update ALU_UC_SHOP_BOT2.py

--- ALU_UC_SHOP_BOT2.py
+++ ALU_UC_SHOP_BOT2.py
@@ -13,7 +13,7 @@ from flask import Flask
 # НАСТРОЙКА
 # ============================================================
 
-BOT_TOKEN = "8660164143:AAGswW4_KWSIQXmqlq8nHWIP2FLhJSFruhs"
+BOT_TOKEN = "8660164143:AAEt1TXap-zqppH819CdWX2P3vwCFT99Hfs"
 
 ADMIN_ID = 6871575684
 
